c9fffffba879038de1ba4c6add4b57e3bc23e3a3
xmejia02@gmail.com <xmejia02@gmail.com>
2025-02-21 15:13:08 -0600

    update AnuraDirect class to use new AdditionalData class, and utilize new custom exceptions

--- src/Anura/AnuraDirect.php
+++ src/Anura/AnuraDirect.php
@@ -7,85 +7,79 @@ use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Client;
 use Exception;
 use Anura\DirectResult;
+use Anura\DirectRequest;
+use Anura\Exceptions\AnuraClientException;
+use Anura\Exceptions\AnuraException;
 
+/**
+ * API client for Anura Direct.
+ */
 class AnuraDirect
 {
     private string $instance;
     private bool $useHttps;
-    private ?string $lastReceivedError;
+    private string $apiUrl;
     private string $source = '';
     private string $campaign = '';
-    private array $additionalData = [];
     private Client $client;
 
     public function __construct(string $instance, bool $useHttps = true)
     {
         $this->instance = $instance;
         $this->useHttps = $useHttps;
+        $this->apiUrl = $useHttps ? 'https://direct.anura.io/direct.json' : 'http://direct.anura.io/direct.json';
         $this->client = new Client();
     }
 
     /**
-     * Gets a result from Anura Direct, or returns null if an error occurred. 
-     * To retrieve the error that occurred, call the getError() method.
+     * Gets a result from Anura Direct.
      */
-    public function getResult(string $ipAddress, string $userAgent = '', string $app = '', string $device = ''): ?DirectResult
+    public function getResult(DirectRequest $request)
     {
         $params = [
             'instance' => $this->instance,
-            'ip' => $ipAddress
+            'ip' => $request->getIpAddress(),
         ];
 
-        if ($userAgent) $params['ua'] = $userAgent;
-        if ($this->source) $params['source'] = $this->source;
-        if ($this->campaign) $params['campaign'] = $this->campaign;
-        if ($app) $params['app'] = $app;
-        if ($device) $params['device'] = $device;
-        $additionalDataString = $this->getAdditionalDataString();
-        if ($additionalDataString) {
-            $params['additional'] = $additionalDataString;
-        }
+        if ($request->getSource()) $params['source'] = $request->getSource();
+        if ($request->getCampaign()) $params['campaign'] = $request->getCampaign();
+        if ($request->getUserAgent()) $params['ua'] = $request->getUserAgent();
+        if ($request->getApp()) $params['app'] = $request->getApp();
+        if ($request->getDevice()) $params['device'] = $request->getDevice();
 
-        $apiUrl = $this->getUrl() . '?' . http_build_query($params);
+        $additionalData = $request->getAdditionalData();
+        if ($additionalData && $additionalData->size() > 0) {
+            $params['additional'] = $additionalData->__toString();
+        }
 
         $response = null;
         try {
-            $response = $this->client->request('GET', $apiUrl);
+            $response = $this->client->request('GET', $this->apiUrl . '?' . http_build_query($params));
         } catch (ClientException $e) {
             $body = json_decode($e->getResponse()->getBody()->getContents(), true);
-            $error = $body['error'] ?? '';
-            $this->lastReceivedError = $error;
-            return null;
-        } catch (ServerException $e) {
-            $this->lastReceivedError = "Anura Server Error: " . $e->getResponse()->getStatusCode();
-            return null;
+            $error = $body['error'] ?? 'Anura Client-Side Error: ' . $e->getResponse()->getStatusCode();
+            throw new AnuraClientException($error);
+        } catch(ServerException $e) {
+            $body = json_decode($e->getResponse()->getBody()->getContents(), true);
+            $error = $body['error'] ?? 'Anura Server-Side Error: ' . $e->getResponse()->getStatusCode();
+            throw new AnuraClientException($error);
         } catch (Exception $e) {
-            $this->lastReceivedError = "Unknown error occurred";
-            return null;
+            throw new AnuraException($e->getMessage());
         }
 
         $result = json_decode($response->getBody()->getContents(), true);
-        $directResult = new DirectResult($result['result'], $result['mobile'], $result['rule_sets'] ?? null, $result['invalid_traffic_type'] ?? null);
+        if (!$result) {
+            throw new AnuraException("Successfully got response from Anura Direct API, but could not parse it.");
+        }
+
+        $directResult = new DirectResult(
+            $result['result'] ?? '',
+            $result['mobile'],
+            $result['rule_sets'] ?? null,
+            $result['invalid_traffic_type'] ?? null
+        );
 
         return $directResult;
-    }
-
-    /**
-     * Returns the last received error from Anura Direct. 
-     * This method will return null if an error has not occurred.
-     */
-    public function getError(): ?string
-    {
-        return $this->lastReceivedError;
-    }
-
-    private function getUrl(): string
-    {
-        if ($this->useHttps) {
-            return 'https://direct.anura.io/direct.json';
-        } else {
-            return 'http://direct.anura.io/direct.json';
-        }
     }
 
     public function getInstance(): string
@@ -118,50 +112,6 @@ class AnuraDirect
         $this->campaign = $campaign;
     }
 
-    public function getAdditionalData(): array
-    {
-        return $this->additionalData;
-    }
-
-    /**
-     * Adds a key/value pair to your additional data. 
-     * If you call addAdditionalData() providing the same $key 
-     * multiple times, the value of additionalData[$key] will 
-     * be updated.
-     */
-    public function addAdditionalData(string $key, string $value): void
-    {
-        $this->additionalData[$key] = $value;
-    }
-
-    /**
-     * Removes a key/value pair from Anura Direct's additional data.
-     */
-    public function removeAdditionalData(string $key): void
-    {
-        if ($this->additionalData[$key]) {
-            unset($this->additionalData[$key]);
-        }
-    }
-
-    /**
-     * Converts an Additional Data associative array into a json-encoded 
-     * string to be used when calling Direct. 
-     */
-    private function getAdditionalDataString(): string
-    {
-        if (count($this->additionalData) <= 0) {
-            return '';
-        }
-
-        $additionalDataString = json_encode($this->additionalData);
-        if (!$additionalDataString) {
-            return '';
-        }
-
-        return $additionalDataString;
-    }
-
     public function getUseHttps(): bool
     {
         return $this->useHttps;
@@ -169,6 +119,7 @@ class AnuraDirect
 
     public function setUseHttps(bool $useHttps): void
     {
+        $this->apiUrl = $useHttps ? 'https://direct.anura.io/direct.json' : 'http://direct.anura.io/direct.json';
         $this->useHttps = $useHttps;
     }
 }
